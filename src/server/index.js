389314c7d5b0e7aa52e90a0667c0e05fbb97ad20
ganeshdemo <87987290+ganeshdemo@users.noreply.github.com>
2021-08-02 16:12:26 +0530

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -12,8 +12,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 
 if (webpackConfig.mode === 'production') {
   app.use(express.static(path.join(__dirname, 'dist')));
-  const routes = require('./routes');
-  app.use(routes());
+  
 } else {
   app.use(middleware(compiler));
   app.use(hotMiddleware(compiler));
@@ -22,12 +21,10 @@ if (webpackConfig.mode === 'production') {
     exclude: [/[\/\\]node_modules[\/\\]/]
   });
 
-  app.use((req, res, next) => {
-    require('./routes').handle(req, res, next);
-  });
+ 
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
