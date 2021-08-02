99c7aa409baafde4de1fa6ad5b55a2a13e5c972a
ganeshdemo <87987290+ganeshdemo@users.noreply.github.com>
2021-08-02 16:12:49 +0530

    Update index.js

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,11 +1,1 @@
-const { RouterNew } = require('express');
 
-const router1 = RouterNew();
-
-router1.get('/foo', (req, res) => res.send('Hello world!'));
-
-module.exports = router1;
-
-if (module.hot) {
-  module.hot.accept();
-}
